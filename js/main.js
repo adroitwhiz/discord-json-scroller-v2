7d68451f399e47995dcb659528eef7ef4d97aa7a
adroitwhiz <adroitwhiz@protonmail.com>
2019-07-24 06:12:47 -0400

    Use Vue for channel list

--- js/main.js
+++ js/main.js
@@ -1,34 +1,54 @@
 const elemIds = {
-	"messageArea":"messages",
-	"channelArea":"channel-list",
-	"renderButton":"render-btn",
-	"messageRenderRangeMin":"msg-range-min",
-	"messageRenderRangeMax":"msg-range-max",
-	"messageJumpNumber":"msg-num",
-	"messageJumpContext":"msg-context",
-	"jumpButton":"jump-btn",
-	"findFilterTextCheckbox":"find-filter-by-text",
-	"findFilterText":"find-filter-text",
-	"findFilterUsernameCheckbox":"find-filter-by-username",
-	"findFilterUsername":"find-filter-username",
-	"findButton":"find-btn",
-	"messageTemplate":"message-template",
-	"channelTemplate":"channel-template",
-	"JSONFilePicker":"json-file-picker"
-},
-      elems = {};
+	'messageArea':'messages',
+	'renderButton':'render-btn',
+	'messageRenderRangeMin':'msg-range-min',
+	'messageRenderRangeMax':'msg-range-max',
+	'messageJumpNumber':'msg-num',
+	'messageJumpContext':'msg-context',
+	'jumpButton':'jump-btn',
+	'findFilterTextCheckbox':'find-filter-by-text',
+	'findFilterText':'find-filter-text',
+	'findFilterUsernameCheckbox':'find-filter-by-username',
+	'findFilterUsername':'find-filter-username',
+	'findButton':'find-btn',
+	'messageTemplate':'message-template',
+	'JSONFilePicker':'json-file-picker'
+};
+const elems = {};
 
 let loadedServer = null;
 let loadedUsers = null;
 
 let activeChannel = null;
-let channelMessages = null;
 
-function jump(array, n, ctx) {return array.slice(Math.max(0, n-ctx), n+ctx+1)};
+const loadServerFromFile = file => {
+	return new Promise((resolve, reject) => {
+		const reader = new FileReader();
+		reader.addEventListener('load', event => {
+			resolve(JSON.parse(event.target.result));
+		});
+		
+		reader.readAsText(file);
+	});
+}
 
-document.addEventListener("DOMContentLoaded", event => {
-	const elems = {};
-	
+document.addEventListener('DOMContentLoaded', event => {
+	const channelList = new Vue({
+		el: '#channel-list',
+		data: {
+			channels: [],
+			activeChannelId: null
+		},
+		methods: {
+			setChannel: channelId => {
+				setActiveChannel(channelId)
+			}
+		}
+	});
+
+	window.channelList = channelList;
+
+	// Bind elements to IDs
 	for (let i in elemIds) {
 		if (elemIds.hasOwnProperty(i)) {
 			let elem = document.getElementById(elemIds[i]);
@@ -38,38 +58,27 @@ document.addEventListener("DOMContentLoaded", event => {
 	}
 	
 	window.messageTemplate = Handlebars.compile(elems.messageTemplate.innerText);
-	window.channelTemplate = Handlebars.compile(elems.channelTemplate.innerText);
 	
-	//window.mt = messageTemplate;
-	
-	elems.JSONFilePicker.addEventListener("change", event => {
-		console.log(event.target.files[0]);
-		
-		if (event.target.files[0]) {
-			var reader = new FileReader();
-			reader.onload = event => {
-				loadedServer = JSON.parse(event.target.result);
-				return reinit();
-				
-			};
-			
-			reader.readAsText(event.target.files[0]);
+	elems.JSONFilePicker.addEventListener('change', event => {
+		const jsonFile = event.target.files[0];
+		if (jsonFile) {
+			loadServerFromFile(jsonFile).then(reinit);
 		}
 	});
 	
-	elems.renderButton.addEventListener("click", event => {
-		let messagesToRender = channelMessages.slice(parseInt(elems.messageRenderRangeMin.value), parseInt(elems.messageRenderRangeMax.value));
+	elems.renderButton.addEventListener('click', event => {
+		let messagesToRender = activeChannel.messages.slice(parseInt(elems.messageRenderRangeMin.value), parseInt(elems.messageRenderRangeMax.value));
 		
 		renderMessages(messagesToRender, messageTemplate, elems.messageArea);
 	});
 	
-	elems.jumpButton.addEventListener("click", event => {
+	elems.jumpButton.addEventListener('click', event => {
 		let jumpID = elems.messageJumpNumber.value;
 		
-		let messageToJumpTo = channelMessages.findIndex(message => message.id === jumpID);
+		let messageToJumpTo = activeChannel.messages.findIndex(message => message.id === jumpID);
 		let messageJumpContext = parseInt(elems.messageJumpContext.value);
 		
-		let messagesToRender = channelMessages.slice(Math.max(0, messageToJumpTo-messageJumpContext), messageToJumpTo+messageJumpContext+1);
+		let messagesToRender = activeChannel.messages.slice(Math.max(0, messageToJumpTo-messageJumpContext), messageToJumpTo+messageJumpContext+1);
 		
 		renderMessages(messagesToRender, elems.messageArea);
 	});
@@ -77,51 +86,21 @@ document.addEventListener("DOMContentLoaded", event => {
 	//elems.findButton
 });
 
-function reinit() {
+function reinit(server) {
+	loadedServer = server;
 	loadedUsers = {};
 	for (let i = 0; i < loadedServer.members.length; i++) {
 		let user = loadedServer.members[i].user;
 		
 		loadedUsers[user.id] = user;
 	}
-	
-	renderChannels(loadedServer.channels, elems.channelArea);
+
+	channelList.channels = loadedServer.channels;
 }
 
-function renderChannels(channels, channelArea) {
-	for (let i = 0; i < loadedServer.channels.length; i++) {
-		const createdElement = document.createElement(null);
-		
-		createdElement.innerHTML = channelTemplate({
-			channelName:loadedServer.channels[i].name
-		});
-		
-		createdElement.addEventListener("click", event => {
-			setActiveChannel(loadedServer.channels[i].name);
-		});
-		
-		createdElement.setAttribute("data-channel", loadedServer.channels[i].name);
-		
-		elems.channelArea.appendChild(createdElement);
-	}
-}
-
-function setActiveChannel(channelName) {
-	activeChannel = channelName;
-	
-	channelMessages = loadedServer.channels.find(channel => channel.name === channelName).messages;
-	
-	const channelElements = elems.channelArea.children;
-	
-	for (let i = 0; i < channelElements.length; i++) {
-		channelElements[i].children[0].classList.remove("selected");
-	}
-	
-	let activeChannelButton = Array.prototype.find.call(channelElements, (elem => elem.getAttribute("data-channel") === channelName));
-	
-	console.log(activeChannelButton);
-	
-	activeChannelButton.children[0].classList.add("selected");
+function setActiveChannel(channelId) {
+	activeChannel = loadedServer.channels.find(channel => channel.id === channelId);
+	channelList.activeChannelId = channelId;
 }
 
 function renderMessages(messages, messageArea) {
@@ -133,9 +112,9 @@ function renderMessages(messages, messageArea) {
 	
 	renderedMessages.reverse();
 	
-	elems.messageArea.innerHTML = renderedMessages.join("");
+	messageArea.innerHTML = renderedMessages.join('');
 	
-	Array.from(elems.messageArea.getElementsByClassName("message-text")).forEach(messageText => {
+	Array.from(messageArea.getElementsByClassName('message-text')).forEach(messageText => {
 		linkifyElement(messageText);
 	});
 }
@@ -145,7 +124,7 @@ function renderMessage(template, message) {
 		text:message.content,
 		attachments:message.attachments,
 		username:loadedUsers[message.author] ? loadedUsers[message.author].username : `<@${message.author}>`,
-		timestamp:moment(parseInt(message.createdTimestamp)).format("MMM D Y h:mm:ss A"),
+		timestamp:moment(parseInt(message.createdTimestamp)).format('MMM D Y h:mm:ss A'),
 		isEdited:message.editedTimestamp !== null,
 		index:message.id
 	});
